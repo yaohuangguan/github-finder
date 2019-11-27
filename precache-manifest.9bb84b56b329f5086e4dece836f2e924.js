aff2c88f4bc26d713daa850c994f3319b49a42b1
yaohuangguan <yaob@miamioh.edu>
2019-11-27 14:52:40 +0800

    Updates

---
+++ precache-manifest.9bb84b56b329f5086e4dece836f2e924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eab3eae3cd785ef2c32f4d2e46a8d4",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "1d92cae1969606c01fe7",
+    "url": "/github-finder/static/css/main.32a3b887.chunk.css"
+  },
+  {
+    "revision": "f77f896d0fd313b23f0d",
+    "url": "/github-finder/static/js/2.9aabb9ec.chunk.js"
+  },
+  {
+    "revision": "1d92cae1969606c01fe7",
+    "url": "/github-finder/static/js/main.c3fc4e38.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
